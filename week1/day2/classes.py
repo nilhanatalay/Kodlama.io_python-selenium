827572108f9d6aa6961c2aa562dc5490a2cd73a5
Nilhan Atalay <118447802+nilhanatalay@users.noreply.github.com>
2023-03-14 14:40:07 +0300

    homework2_classes

---
+++ week1/day2/classes.py
@@ -0,0 +1,16 @@
+# özellik barındıran nesneler - operasyon barındıran nesneler
+# Python case sensitive'dir.
+class Banka:
+      def krediBasvur(self):
+            print("Kredi başvurusu yapıldı.")
+
+      def krediHesapla(self):
+            print ("Hesaplar Yapıldı")
+
+
+  # krediHesapla() burada (class içinde) kullanınca self gerekmedi. Class dışından kullanırken self gerekti.
+
+banka = Banka()
+banka.krediBasvur()
+
+#self = this
